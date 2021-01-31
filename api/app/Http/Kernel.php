1d331c10a889459844bfdfcce57ec4dc8e8b0ecd
wadanaoumi <wada121400@gmail.com>
2021-02-01 01:10:39 +0900

    s

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:api',
+            \Fruitcake\Cors\HandleCors::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
